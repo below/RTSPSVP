ff73ae1f58e0e4e255692e65bf0468749ac48bb8
Alexander von Below <Alex@vonBelow.Com>
2016-06-23 16:41:32 +0200

    Initial Commit

---
+++ RTSPSVP/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RTSPSVP
+//
+//  Created by Alexander v. Below on 23.06.16.
+//  Copyright © 2016 Deutsche Telekom AG. All rights reserved.
+//
+
+import UIKit
+import MobileVLCKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var view: VLCVideoView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
