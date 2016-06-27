872ebf85836ae93cb2aaba51c7a6880429b7388b
Alexander von Below <Alex@vonBelow.Com>
2016-06-27 12:06:12 +0200

    Thanks to @neonacho (and trying the same thing in Objective-C), this is how it works

--- RTSPSVP/ViewController.swift
+++ RTSPSVP/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
-import MobileVLCKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VLCMediaPlayerDelegate{
 
-    @IBOutlet weak var view: VLCVideoView!
+    @IBOutlet weak var videoView: UIView!
+    var player : VLCMediaPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.player = VLCMediaPlayer()
+        self.player.delegate = self;
+        self.player.drawable = self.view;
+        
+        if let url = URL(string:"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4") {
+            /* create a media object and give it to the player */
+            let media = VLCMedia(url: url)
+            self.player.media = media;
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +32,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playPause(_ sender: AnyObject) {
+        if self.player.isPlaying {
+            self.player.pause()
+        }
+        else {
+            self.player.play()
+        }
+    }
 
 }
 
